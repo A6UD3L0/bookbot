b9c5d48a6261fb56fe29d9c59fd516ab8e792680
A6UD3L0 <juanf.agudelor@gmail.com>
2025-05-05 15:20:22 -0500

    version final

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+import sys
 
-from stats import count_words, count_characters , create_report
+from stats import count_characters, count_words, create_report
 
-def read_book(file_path): 
-    with open(file_path) as f: 
-        file_content= f.read()
+
+def read_book(file_path):
+    with open(file_path) as f:
+        file_content = f.read()
         return file_content
 
 
-def main(file_path): 
-    text_content=read_book(file_path)
-    print(text_content)
-    word_count=count_words(text_content)
-    print(word_count)
-    character_dict=count_characters(text_content)
-    print(character_dict)
-    report=create_report(file_path, word_count, character_dict)
+def main(file_path):
+    text_content = read_book(file_path)
+    word_count = count_words(text_content)
+    character_dict = count_characters(text_content)
+    report = create_report(file_path, word_count, character_dict)
     print(report)
 
-path_fr="books/frankenstein.txt"
 
-main(path_fr)
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
+
+main(book_path)
--- stats.py
+++ stats.py
@@ -1,20 +1,32 @@
-def count_words(text_content): 
-    word_count=len(text_content.split(" "))
-    return word_count 
+# stats.py
 
-def count_characters(text_content): 
-    text_content_lower=text_content.lower()
-    character_dict={}
-    for char in text_content_lower: 
-        if char in character_dict.keys(): 
+
+def count_words(text_content):
+    return len(text_content.split())
+
+
+def count_characters(text_content):
+    text_content_lower = text_content.lower()
+    character_dict = {}
+    for char in text_content_lower:
+        if char in character_dict.keys():
             character_dict[char] += 1
-        else:  
+        else:
             character_dict[char] = 1
     return character_dict
 
-def create_report(book_path,word_count,character_dict):
-    character_dict_sorted=character_dict.sort(reverse=True, key=sort_on)
-    nice_word_count=f"Found {word_count} total words" 
-    report= book_path + nice_word_count + character_dict_sorted
-    return report 
-    
+
+def create_report(book_path, word_count, character_dict):
+    report = (
+        "----------- Word Count ----------\n"
+        f"Found {word_count} total words\n"
+        "--------- Character Count -------\n"
+    )
+
+    sorted_chars = sorted(
+        character_dict.items(), key=lambda item: item[1], reverse=True
+    )
+    for char, count in sorted_chars:
+        report += f"{char}: {count}\n"
+
+    return report
